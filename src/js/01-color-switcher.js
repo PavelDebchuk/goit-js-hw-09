0470ee5853e267492864b60e8ac48527afe96c23
Pavel <Pavel.Debchuk@gmail.com>
2021-11-30 22:03:12 +0300

    dz 1/3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const buttonStart = document.querySelector('[data-start]');
 
+
+const color = document.body;
+let timerRandom;
+
+buttonStart.addEventListener('click', getRandomHexColor);
+function getRandomHexColor() {
+  return timerRandom = setInterval(() => {
+        buttonStart.disabled = true;
+    console.log('play random');
+    color.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  },1000);
+}
+
+const buttonStop = document.querySelector('[data-stop]');
+buttonStop.addEventListener('click', stopRandom);
+function stopRandom() {
+  clearInterval(timerRandom);
+  buttonStart.disabled = false;
+  console.log('stop random')
+}
